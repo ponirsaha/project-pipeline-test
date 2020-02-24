7bfce146d2a5adb63e584e9c8d623a659d7613c0
Ponir Saha <ponir.kumer@datasoft-bd.com>
2020-02-25 05:33:48 +0600

    first commit

---
+++ src/main/java/com/project/pipeline/projectpipelinetest/ProjectPipelineTestApplication.java
@@ -0,0 +1,13 @@
+package com.project.pipeline.projectpipelinetest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectPipelineTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectPipelineTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/pipeline/projectpipelinetest/ProjectPipelineTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.pipeline.projectpipelinetest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectPipelineTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
